04ec01dc4b0df7040cd0e258361d61210539f261
Hajk <tunczorx@gmail.com>
2018-11-05 13:56:03 +0100

    My first form which allow people anwser if they gonna take part in party.

---
+++ Pro_ASP.NET_MVC_5_PLATFORM_A.Freeman_V1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pro_ASP.NET_MVC_5_PLATFORM_A.Freeman_V1.Models;
+
+namespace Pro_ASP.NET_MVC_5_PLATFORM_A.Freeman_V1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczór";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //todo: wyslij zawartosc guestResponse do organiatora przyjecia
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //błąd kontroli poprawności, więc ponownie wyświetlany formularz wprowadzania danych
+                return View();
+            }
+        }
+    }
+}
---
+++ Pro_ASP.NET_MVC_5_PLATFORM_A.Freeman_V1/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Pro_ASP.NET_MVC_5_PLATFORM_A.Freeman_V1.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Proszę podać swoje imię i naziwsko.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać adres e-mail.")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Proszę podać prawidłowy adres e-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać numer telefonu.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Proszę określić, czy weźmiesz udział.")]
+        public bool? WillAtend { get; set; }
+    }
+}
